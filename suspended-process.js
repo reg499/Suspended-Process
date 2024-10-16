2939e9053d546654f1407c6a36ce5c5f25ac7917
reg499 <newregtemp@outlook.com>
2024-10-16 15:34:01 +0400

    suspended-process.js

---
+++ suspended-process.js
@@ -0,0 +1,13 @@
+//const uint32 = ref.types.uint32;
+// const kernel32 = ffi.Library("kernel32", {
+//   SuspendThread: [uint32, ["pointer"]],
+// });
+
+// console.log("Suspending thread...");
+// const suspendResult = kernel32.SuspendThread(pi.hThread);
+
+// if (suspendResult === 0xffffffff) {
+//   console.log("Failed to suspend thread.");
+//   throw new Error("SuspendThread failed.");
+// }
+// console.log("Thread suspended successfully.");
